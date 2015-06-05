ca5681099d66570b85ad72d0d0b17809c13f09b7
Kenneth Richardson <krichardson1318@gmail.com>
2015-06-05 19:22:49 -0400

    Updating statemachine to transform to utm coords

--- src/turtlebot_smach/src/simple_statemachine.py
+++ src/turtlebot_smach/src/simple_statemachine.py
@@ -14,6 +14,7 @@ from smach_ros import SimpleActionState
 from std_srvs.srv import Empty
 from waypoint_manager_msgs.srv import NextWaypoint
 from waypoint_manager_msgs.srv import PushWaypoint
+from geodesy import utm
 
 
 class Idle(smach.State):
@@ -48,13 +49,23 @@ class MovingToWaypoint(smach.State):
         smach.State.__init__(self, outcomes=['succeeded', 'failed'], input_keys=['waypoint_in'])
 
     def execute(self, userdata):
-        rospy.loginfo("In MovingToWaypoint")
         move_base_client = SimpleActionClient('move_base', MoveBaseAction)
-        move_base_client.wait_for_server(rospy.Duration(5))
+        #move_base_client.wait_for_server(rospy.Duration(5))
         goal_pose = MoveBaseGoal()
-        goal_pose.target_pose.header.frame_id = 'map'
+        goal_pose.target_pose.header.frame_id = 'utm'
         goal_pose.target_pose.header.stamp = rospy.Time.now()
-        goal_pose.target_pose.pose = userdata.waypoint_in
+
+        # First point specified is latitude, second is longitude
+        utm_waypoint = utm.fromLatLong(userdata.waypoint_in.position.x, userdata.waypoint_in.position.y, 250)
+        rospy.loginfo(utm_waypoint)
+        goal_pose.target_pose.pose.position.x = utm_waypoint.easting
+        goal_pose.target_pose.pose.position.y = utm_waypoint.northing
+        goal_pose.target_pose.pose.position.z = 0
+        goal_pose.target_pose.pose.orientation.x = 0
+        goal_pose.target_pose.pose.orientation.y = 0
+        goal_pose.target_pose.pose.orientation.z = 0
+        goal_pose.target_pose.pose.orientation.w = 1
+
         move_base_client.send_goal(goal_pose)
         move_base_client.wait_for_result()
         result = move_base_client.get_state()
@@ -69,7 +80,6 @@ class AdvanceWaypoint(smach.State):
         smach.State.__init__(self, outcomes=['succeeded'])
 
     def execute(self, userdata):
-        rospy.loginfo("In AdvanceWaypoint")
         advance_waypoint_proxy = rospy.ServiceProxy('advance_waypoint', Empty)
         advance_waypoint_proxy()
         return 'succeeded'
